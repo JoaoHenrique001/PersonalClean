ed81e060f8f45ec3a960d4dd81fc620600cad0e9
joaoHenrique001 <joaogamesbrbrazilhotmail9@gmail.com>
2025-04-30 16:28:08 +0200

    area de administradores

---
+++ area_administradores.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administrador | Personal Clean</title>
+
+    <!--enlace de css-->
+    <link rel="stylesheet" href="./assets/estilos.css">
+    <!--enlace de css-->
+
+    <!--inicio logo de la pagina ventana-->
+    <link rel="icon" type="image/ico" href="./assets/images/logo.ico">
+    <!--fin logo de la pagina ventana-->
+
+</head>
+<body>
+    
+</body>
+</html>
